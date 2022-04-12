22c90f1b3a1899765901e167fc6966a3c88cee68
gaelle <g.kerforne@gmail.com>
2022-04-12 20:03:57 +0200

    page profil utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Entity\Recette;
+use App\Entity\User;
+use App\Repository\CompositionRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\RecetteRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AccountController extends AbstractController
 {
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(
+        RecetteRepository $recetteRepository,
+        IngredientRepository $ingredientRepository,
+        CompositionRepository $compositionRepository
+    ): Response
     {
-        return $this->render('account/index.html.twig');
+
+        return $this->render('account/index.html.twig',[
+            'ingredient' => $ingredientRepository->findAll(),
+            'compositions'=> $compositionRepository->findAll(),
+            'recette' => $recetteRepository->findAll(),
+
+        ]);
     }
 }
