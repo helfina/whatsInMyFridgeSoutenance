9d950ad97c8bd6a35648a9dfa8c31e577f9dd9e4
Erwan <erwanhumblot02@gmail.com>
2022-04-12 11:49:17 +0200

    avis form et affichage ingredient form chemin recette

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
 use App\Repository\IngredientRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\IngredientType;
 
 class IngredientController extends AbstractController
 {
@@ -18,4 +22,50 @@ class IngredientController extends AbstractController
 
         ]);
     }
+    
+    #[Route('/ingredient/ajout', name: 'app_ingredient_ajout')]
+    public function ajout(IngredientRepository $ir, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $ingredient = new Ingredient();
+        $ingredientForm = $this->createForm(IngredientType::class, $ingredient);
+
+        $ingredientForm->handleRequest($request);
+
+        if($ingredientForm->isSubmitted() && $ingredientForm->isValid()){
+
+            if($fichier = $ingredientForm->get('photo')->getData()){
+                $nomFichier = pathinfo($fichier->getClientOriginalName(), PATHINFO_FILENAME);
+                $nomFichier = str_replace(" ", "_", $nomFichier);
+                $nomFichier .= '_'.uniqid().'.'.$fichier->guessExtension();
+                $fichier->move("img", $nomFichier);
+                $ingredient->setPhoto($nomFichier);
+            }
+            
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Votre ingredient a bien été ajouté');
+
+            return $this->redirectToRoute('app_ingredient_detail', ['id' => $ingredient->getId()]);
+
+        }
+        return $this->render('ingredient/ajout.html.twig', [
+            'controller_name' => 'IngredientController',
+            'ingredientForm' => $ingredientForm->createView()
+        ]);
+    
+    }
+
+    #[Route('/ingredient/{id}', name: 'app_ingredient_detail', requirements:['id' => '[0-9]+'])]
+    public function detail(IngredientRepository $ir, $id): Response
+    {
+        return $this->render('ingredient/detail.html.twig', [
+            'controller_name' => 'IngredientController',
+            'ingredient' => $ir->find($id)
+        ]);
+    }
+
+   
 }
---
+++ src/Form/AvisType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'label' => 'Votre commentaire',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('envoyer', SubmitType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
