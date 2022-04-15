4be4b73b54e421bd977aa5b5c88c95831e8372f4
Arthur <arthur.vives@gmail.com>
2022-04-15 15:26:22 +0200

    NOT MERGE

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Form\SearchingredientType;
-use App\Repository\CompositionRepository;
 use App\Repository\RecetteRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\CompositionRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,6 +27,34 @@ class HomeController extends AbstractController
         ]);
     }
 
+
+    #[Route('/search', name: 'app_search', methods: 'GET')]
+    public function searchAction(Request $request, IngredientRepository $ir)
+    {
+       
+  
+        $requestString = $request->get('q');
+  
+        $entities =  $ir->findBySearch($requestString);
+  
+        if(!$entities) {
+            $result['entities']['error'] = "ingrédient non disponible";
+        } else {
+            
+            $result['entities'] = $this->getRealEntities($entities);
+        }
+  
+        return new Response(json_encode($result));
+    }
+  
+    public function getRealEntities($entities){
+  
+        foreach ($entities as $entity){
+            $realEntities[$entity->getId()] = $entity->getName();
+        }
+  
+        return $realEntities;
+    }
 }
 
 
