19d1d6e88ec788bf57f5af47bdba4ae48177f68e
gaelle kerforne <stage.seeweb@gmail.com>
2022-04-18 16:57:05 +0200

    ajout migration gitignor et recupration de la branche main
    Took 19 minutes

---
+++ src/Controller/Admin/Contact2CrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Contact;
+
+use App\Entity\Contact;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class Contact2CrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Contact::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
